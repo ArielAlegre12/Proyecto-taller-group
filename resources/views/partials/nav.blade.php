81c038a579adc6efcb6579ccf92c93d8a1853dab
ArielAlegre12 <114325019+ArielAlegre12@users.noreply.github.com>
2026-04-19 13:50:36 -0300

    cambie-carrito-login-en-nav
    más que nada en pantallas chicas, para que el carrito quede fuera del menu hamburguesa. y que el boton de login se muestre dentro.

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -6,18 +6,30 @@
             <div class="logo-circle">
                 <i class="bi bi-clipboard-pulse"></i>
             </div>
-            <span class="fw semibold-text-success">Huellas Felices</span>
+            <span class="brand-text">Huellas Felices</span>
         </a>
 
-        <!--btn mobile-->
-        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <i class="bi bi-list fs-3"></i>
-        </button>
+        <!--derecha(mobile)carrito+toggler--->
+        <div class="d-flex align-items-center gap-3 ms-auto">
 
-        <!---contenido-->
+            <!--carrito-->
+            <div class="position-relative d-lg-none">
+                <a href="#" class="text-dark fs-5">
+                    <i class="bi bi-cart carrito"></i>
+                </a>
+                <span class="cart-badge">0</span>
+            </div>
+
+            <!--toggler--->
+            <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <i class="bi bi-list fs-3"></i>
+            </button>
+        </div>
+
+        <!--collapse(links) + pantalla desktop--->
         <div class="collapse navbar-collapse" id="navbarNav">
 
-            <!--links-->
+            <!--links--->
             <ul class="navbar-nav mx-auto gap-lg-4 text-center">
 
                 <li class="nav-item">
@@ -31,20 +43,24 @@
                 <li class="nav-item">
                     <a href="/tienda" class="nav-link {{ request()->is('tienda') ? 'active' : '' }}">Tienda</a>
                 </li>
+                <!--login mobile oculto en desktop-->
+                <div class="d-lg-none mt-3 px-3">
+                    <a href="/login" class="btn-login w-100 text-center d-block">Login</a>
+                </div>
             </ul>
 
-            <!--derecha-->
-            <div class="d-flex align-items-center gap-3 justify-content-center mt-3 mt-lg-0">
+            <!--derecha desktop-->
+            <div class="d-none d-lg-flex align-items-center gap-3">
 
-                <!--carrito-->
+                <!--carrito desktop--->
                 <div class="position-relative">
                     <a href="#" class="text-dark fs-5">
-                        <i class="carrito bi bi-cart3"></i>
+                        <i class="bi bi-cart3 carrito"></i>
                     </a>
                     <span class="cart-badge">0</span>
                 </div>
 
-                <!--btn login-->
+                <!---login desktop--->
                 <a href="/login" class="btn btn-success px-4 rounded-3">
                     Login
                 </a>
