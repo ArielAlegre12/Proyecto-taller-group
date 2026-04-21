9c9ce55b569937fcc22e880585d915ec0db6ed2a
ArielAlegre12 <114325019+ArielAlegre12@users.noreply.github.com>
2026-04-21 17:38:20 -0300

    Update comercializacion.blade.php

--- resources/views/pages/comercializacion.blade.php
+++ resources/views/pages/comercializacion.blade.php
@@ -9,16 +9,4 @@
 @endsection
 
 @section('content')
-    <h4>Formas de pago</h4>
-    <ul>
-        <li>Tarjetas de crédito y débito</li>
-        <li>Transferencia bancaria</li>
-        <li>Efectivo en el local</li>
-    </ul>
-
-    <h4>Envíos</h4>
-    <p>Realizamos envíos a todo el país</p>
-
-    <h4>Retiro</h4>
-    <p>Podés retirar tu compra en nuestro local sin costo</p>
 @endsection
